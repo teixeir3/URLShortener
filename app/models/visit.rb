389a8ba4c858caeca66977e4e2042a4e18c8518b
CJ Avilla <cjavilla@gmail.com>
2013-12-05 17:06:20 -0800

    fixing visitor association

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -5,7 +5,10 @@ class Visit < ActiveRecord::Base
   validates :shortened_url_id, :presence => true
 
   belongs_to :shortened_url
-  belongs_to :visitor
+  belongs_to :visitor,
+             :primary_key => :id,
+             :foreign_key => :user_id,
+             :class_name => "User"
 
   def self.record_visit!(user, shortened_url)
     Visit.create!(
