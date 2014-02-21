b2c205b31f5d7986cebc5194bb3a4fb42386b3b7
CJ Avilla <cjavilla@gmail.com>
2014-02-21 10:17:49 -0800

    testing rails 4 stuff

--- db/schema.rb
+++ db/schema.rb
@@ -9,50 +9,37 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130125155824) do
+ActiveRecord::Schema.define(version: 20130124213836) do
 
-  create_table "shortened_urls", :force => true do |t|
-    t.string   "long_url",     :null => false
-    t.string   "short_url",    :null => false
-    t.integer  "submitter_id", :null => false
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+  create_table "shortened_urls", force: true do |t|
+    t.string   "long_url",     null: false
+    t.string   "short_url",    null: false
+    t.integer  "submitter_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "shortened_urls", ["short_url"], :name => "index_shortened_urls_on_short_url", :unique => true
-  add_index "shortened_urls", ["submitter_id"], :name => "index_shortened_urls_on_submitter_id"
+  add_index "shortened_urls", ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
+  add_index "shortened_urls", ["submitter_id"], name: "index_shortened_urls_on_submitter_id"
 
-  create_table "taggings", :force => true do |t|
-    t.integer  "shortened_url_id"
-    t.integer  "tag_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "tags", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
-  create_table "users", :force => true do |t|
-    t.string   "email",      :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-
-  create_table "visits", :force => true do |t|
+  create_table "visits", force: true do |t|
     t.integer  "user_id"
     t.integer  "shortened_url_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "visits", ["shortened_url_id"], :name => "index_visits_on_shortened_url_id"
-  add_index "visits", ["user_id"], :name => "index_visits_on_user_id"
+  add_index "visits", ["shortened_url_id"], name: "index_visits_on_shortened_url_id"
+  add_index "visits", ["user_id"], name: "index_visits_on_user_id"
 
 end
